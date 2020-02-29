50b3e54f12f68446863ba3f0247c6afacebf2b9a
Manuchehr Mirzoev <m.m.manuchehri1@gmail.com>
2020-02-29 15:51:11 -0500

    loop updated

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -9,6 +9,7 @@ public class LoopPractice {
         }
         for (int i = 0; i < 20; i+=5) {
             System.out.println("i = " + i);
+            System.out.println("hello");
         }
     }
 }
