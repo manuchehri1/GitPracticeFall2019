e2b699882d5145f699c4928553e87a932b5e1ccc
Manuchehr Mirzoev <m.m.manuchehri1@gmail.com>
2020-02-29 15:42:09 -0500

    another loop is added

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -1,4 +1,14 @@
 package day1;
 
 public class LoopPractice {
+    public static void main(String[] args) {
+
+        // some random code
+        for (int i=1; i<10; i++) {
+            System.out.println("counter = " + i);
+        }
+        for (int i = 0; i < 20; i+=5) {
+            System.out.println("i = " + i);
+        }
+    }
 }
