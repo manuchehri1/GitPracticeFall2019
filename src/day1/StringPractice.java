3ba528fc2208dfa3a11ef4aa1afbbb9ab41abb64
Manuchehr Mirzoev <m.m.manuchehri1@gmail.com>
2020-02-29 13:44:33 -0500

    my first commit

---
+++ src/day1/StringPractice.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class StringPractice {
+    public static void main(String[] args) {
+
+        String firstName = "Manuchehr";
+        String lastName = "Mirzoev";
+        System.out.println("full name = " + firstName+" "+lastName);
+
+
+    }
+}
