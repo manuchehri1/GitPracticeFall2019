cef86b4235b6531f8d74233f6addfa1295071ad8
Manuchehr Mirzoev <m.m.manuchehri1@gmail.com>
2020-04-01 03:30:54 -0400

    code changed

--- src/day1/PracticeBranch.java
+++ src/day1/PracticeBranch.java
@@ -3,5 +3,6 @@ package day1;
 public class PracticeBranch {
     public static void main(String[] args) {
         System.out.println("This is first code");
+        System.out.println("the code is changed");
     }
 }
